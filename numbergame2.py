7e100bcec886c031d44ea8935fb56ddf68163552
2kwattz <37065373+2kwattz@users.noreply.github.com>
2020-08-12 14:33:46 +0530

    Update numbergame2.py

--- numbergame2.py
+++ numbergame2.py
@@ -1,8 +1,7 @@
-import keyboard
 import pyfiglet
 import random
 
-number = random.randint(0,9)
+number = random.randint(0,100)
 no_of_guesses = 0
 
 banner = pyfiglet.figlet_format("Guess The Number")
@@ -11,10 +10,8 @@ print("Game by 2kwattz \nTries Left:10")
 
 while (no_of_guesses<10):
     no_of_guesses = no_of_guesses + 1
+
     guess = int(input("Enter the number\n"))
-    if no_of_guesses>9:
-        print("Game Over\n")
-        break
 
     if guess>number:
         print(f"Enter a smaller number , {no_of_guesses} tries done")
@@ -26,8 +23,7 @@ while (no_of_guesses<10):
         print(f"Congratulations! You guessed the number correct! \n The number is {number} \n")
         break
     else:
-        print("Please enter a valid number\n")
+        print("Enter a valid number\n")
         break
-
-        print("Program end . Press ESC to continue\n")
-        keyboard.wait("esc")
+    if no_of_guesses>9:
+        print("GAME OVER\n")
